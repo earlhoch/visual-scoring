c26cdf0ea995bdba808a46761363d270b9400d58
Joshua Hochuli <jeh176@pitt.edu>
2016-05-06 21:10:01 -0400

    center_around argument added

--- removal.py
+++ removal.py
@@ -4,15 +4,19 @@ import copy
 import sys
 import argparse
 
-parser = argparse.ArgumentParser(description="Remove each atom from a molecule, and output each new molecule to .sdf")
+parser = argparse.ArgumentParser(description="Remove each non-hydrogen atom from a molecule, and output each new molecule to .sdf")
 parser.add_argument('file',type=str, help = 'pdb file containing molecule')
 parser.add_argument('-s','--size', type=float, help = 'edge length of bounding cube')
+parser.add_argument('--center_around', type=str, metavar = 'FILE', help='pdb file containing molecule to center removal cube')
 parser.add_argument('-c', '--center', type = float, nargs=3, metavar =
         ('X','Y','Z'),help = 'coordinates to center bounding cube (default [0,0,0])')
 args = parser.parse_args()
 
-if args.center and args.size is None:
-    parser.error("--center requires --size")
+if args.size is None:
+    if args.center:
+        parser.error("--center requires --size")
+    if args.center_around:
+        parser.error("--center_around requires --size")
 
 obConversion = openbabel.OBConversion()
 obConversion.SetInAndOutFormats("pdb", "sdf")
@@ -21,8 +25,23 @@ obConversion.ReadFile(mol, args.file)
 mol = pybel.Molecule(mol)
 mol.OBMol.AddHydrogens()
 
+def center(mol):
+    xTotal = 0.0
+    yTotal = 0.0
+    zTotal = 0.0
+    for atom in mol:
+        xTotal+= atom.OBAtom.GetX()
+        yTotal+= atom.OBAtom.GetY()
+        zTotal+= atom.OBAtom.GetZ()
+
+    count = mol.OBMol.NumAtoms()
+    newX = xTotal / count
+    newY = yTotal/count
+    newZ = zTotal/count
+
+    return (newX,newY,newZ)
+
 def genRemovalSdf(mol):
-    print("No bounds input, removing every atom")
     fileName = mol.title.partition(".")[0]+".sdf"
     sdfOut = pybel.Outputfile("sdf", fileName, overwrite = True)
 
@@ -44,7 +63,6 @@ def genRemovalSdf(mol):
             removedCounter += 1
             sdfOut.write(newMol)
     print("\n"+str(removedCounter)+" atoms removed")
-    
 
 def genRemovalSdfCube(mol,size,x=0,y=0,z=0):
     allowedDist = float(size)/2
@@ -56,7 +74,7 @@ def genRemovalSdfCube(mol,size,x=0,y=0,z=0):
     validCounter=0
     removedCounter=0
     for atom in mol:
-        sys.stdout.write("Checking atom "+str(counter)+"/"+str(atomTotal)+"\r")
+        sys.stdout.write("Checking atoms: "+str(counter)+"/"+str(atomTotal)+"\r")
         sys.stdout.flush()
         valid = False
         if atom.OBAtom.GetX() < x+allowedDist: #positive bounds
@@ -84,23 +102,23 @@ def genRemovalSdfCube(mol,size,x=0,y=0,z=0):
     else:
         print(str(removedCounter)+" atoms removed")
 
-def test(mol):
-    mol=pybel.Molecule(mol)
-    print(mol.OBMol.NumAtoms())
-    print(mol.OBMol.AddHydrogens())
-    print(mol.OBMol.NumAtoms())
-    print(mol.OBMol.DeleteHydrogens())
-    print(mol.OBMol.NumAtoms())
-
-#test(mol)
-
 if(args.size):
     if(args.center):
         print("Removing cube of edge length " +str(args.size)+ " around point ["+str(args.center[0])+ ", "+str(args.center[1])+", "+str(args.center[2])+"]")
         genRemovalSdfCube(mol, args.size, args.center[0], args.center[1], args.center[2])
+    elif(args.center_around):
+        print("Removing cube of edge length "+str(args.size)+ " around "+args.center_around)
+        cen = openbabel.OBMol()
+        obConversion.ReadFile(cen, args.center_around)
+        cen = pybel.Molecule(cen)
+        mol.OBMol.AddHydrogens()
+        cenCoords = center(cen)
+
+        genRemovalSdfCube(mol, args.size,cenCoords[0],cenCoords[1],cenCoords[2] )
     else:
-        print("Removing cube of length " +str(args.size)+ " around point [0,0,0]")
+        print("Removing cube of edge length " +str(args.size)+ " around point [0,0,0]")
         genRemovalSdfCube(mol, args.size)
 else:
+    print("No bounds input, removing every atom")
     genRemovalSdf(mol)
 
